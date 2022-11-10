4728078ac4547c5a7dad7db0370d4890a06c1c6f
JmJunior20 <jmjuniormonteiro@gmail.com>
2022-11-10 17:55:57 -0300

    Definindo quando o backToTopButton vai aparecer

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
+window.addEventListener('scroll', onScroll)
+
 function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll ()
+}
+
+function showNavOnScroll() {
     if (scrollY > 0) {
         navigate.classList.add('scroll')
     } else {
         navigate.classList.remove('scroll')
+    }
+}
+
+function showBackToTopButtonOnScroll () {
+    if (scrollY > 350) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
     }
 }
 
