ef2b56debcd10167cc00c243ae4f5e207d59a878
JmJunior20 <jmjuniormonteiro@gmail.com>
2022-11-10 21:00:57 -0300

    Criando a responsividade do site

--- main.js
+++ main.js
@@ -1,31 +1,58 @@
-window.addEventListener('scroll', onScroll)
+window.addEventListener("scroll", onScroll);
 
-onScroll()
+onScroll();
 function onScroll() {
-    showNavOnScroll()
-    showBackToTopButtonOnScroll ()
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+  activeMenuAtCurrentSection();
+
+//   activeMenuAtCurrentSection(home)
+//   activeMenuAtCurrentSection(jogos)
+//   activeMenuAtCurrentSection(galeria)
+//   activeMenuAtCurrentSection(contato)
 }
+
+// function activeMenuAtCurrentSection(section) {
+//     const targetLine = scrollY + innerHeight / 2
+
+//     const sectionTop = section.offsetHeigth
+//     const sectionHeight = section.offsetHeigth
+//     const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+//     const sectionEndAt = sectionTop + sectionHeight
+//     const sectionEndPassedTargetline = sectionEndAt <= targetLine
+
+//     const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+//     const sectionId = section.getAttribute('id')
+//     const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+//     menuElement.classList.remove('active')
+//     if (sectionBoundaries) {
+//         menuElement.classList.add('active')
+//     }
+// }
 
 function showNavOnScroll() {
-    if (scrollY > 0) {
-        navigate.classList.add('scroll')
-    } else {
-        navigate.classList.remove('scroll')
-    }
+  if (scrollY > 0) {
+    navigate.classList.add("scroll");
+  } else {
+    navigate.classList.remove("scroll");
+  }
 }
 
-function showBackToTopButtonOnScroll () {
-    if (scrollY > 350) {
-        backToTopButton.classList.add('show')
-    } else {
-        backToTopButton.classList.remove('show')
-    }
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 350) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
 }
 
 function openMenu() {
-    document.body.classList.add('menu-expanded')
+  document.body.classList.add("menu-expanded");
 }
 
 function closeMenu() {
-    document.body.classList.remove('menu-expanded')
+  document.body.classList.remove("menu-expanded");
 }
